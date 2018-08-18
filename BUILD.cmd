bb2242672fb43414d0fa547e6d41c19ee5abcb40
Emilian Cioca <ciocaemilian@hotmail.ca>
2018-08-18 16:55:31 -0400

    BUILD script can now select to build only Debug or Release
    - Reduced the verbosity of the BUILD script

--- BUILD.cmd
+++ BUILD.cmd
@@ -1,10 +1,14 @@
 @echo off
 
-call :BuildConfiguration Debug Win32 || goto :eof
-call :BuildConfiguration Release Win32 || goto :eof
-call :BuildConfiguration Debug "Any CPU" || goto :eof
-call :BuildConfiguration Release "Any CPU" || goto :eof
-call :BuildConfiguration ReleaseWithExceptions Win32 || goto :eof
+if not "%1" == "-r" (
+	call :BuildConfiguration Debug Win32 || goto :eof
+	call :BuildConfiguration Debug "Any CPU" || goto :eof
+)
+if not "%1" == "-d" (
+	call :BuildConfiguration Release Win32 || goto :eof
+	call :BuildConfiguration Release "Any CPU" || goto :eof
+	call :BuildConfiguration ReleaseWithExceptions Win32 || goto :eof
+)
 
 title Done!
 echo Done!
@@ -13,7 +17,7 @@ goto :eof
 :BuildConfiguration
 title Building %1 - %2
 echo ^>^>^>^>^>^> Building %1 - %2 ^<^<^<^<^<^<
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSbuild.exe" /t:Build /p:Configuration=%1 /property:Platform=%2 "Jewel3D.sln"
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSbuild.exe" /v:m /t:Build /p:Configuration=%1 /property:Platform=%2 "Jewel3D.sln"
 if errorlevel 1 (
    title Build Failure %1 - %2
    echo ^>^>^>^>^>^> Build Failure %1 - %2 ^<^<^<^<^<^<
