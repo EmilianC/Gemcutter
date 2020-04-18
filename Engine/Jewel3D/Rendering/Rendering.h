tersers/Jewel3D

Added C++ Reflection

Engine/Jewel3D/Rendering/Rendering.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2017 Emilian Cioca
 #pragma once
+#include "Jewel3D/Reflection/Reflection.h"
 #include "Jewel3D/Utilities/Singleton.h"
 
 #include <string>
@@ -149,3 +150,65 @@ namespace Jwl
 		unsigned MaxDrawBuffers = 0;
 	} &GPUInfo = Singleton<class GPUInfo>::instanceRef;
 }
+
+REFLECT(Jwl::TextureFormat)<>,
+	VALUES <
+		REF_VALUE(RGB_8),
+		REF_VALUE(RGB_16),
+		REF_VALUE(RGB_16F),
+		REF_VALUE(RGB_32),
+		REF_VALUE(RGB_32F),
+		REF_VALUE(RGBA_8),
+		REF_VALUE(RGBA_16),
+		REF_VALUE(RGBA_16F),
+		REF_VALUE(RGBA_32),
+		REF_VALUE(RGBA_32F),
+		REF_VALUE(DEPTH_24)
+	>
+REF_END;
+
+REFLECT(Jwl::TextureWrapMode)<>,
+	VALUES <
+		REF_VALUE(Clamp),
+		REF_VALUE(ClampWithBorder),
+		REF_VALUE(Repeat),
+		REF_VALUE(RepeatMirrored),
+		REF_VALUE(RepeatMirroredOnce)
+	>
+REF_END;
+
+REFLECT(Jwl::TextureFilterMode)<>,
+	VALUES <
+		REF_VALUE(Point),
+		REF_VALUE(Linear),
+		REF_VALUE(Bilinear),
+		REF_VALUE(Trilinear)
+	>
+REF_END;
+
+REFLECT(Jwl::CullFunc)<>,
+	VALUES <
+		REF_VALUE(None),
+		REF_VALUE(Clockwise),
+		REF_VALUE(CounterClockwise)
+	>
+REF_END;
+
+REFLECT(Jwl::BlendFunc)<>,
+	VALUES <
+		REF_VALUE(None),
+		REF_VALUE(Linear),
+		REF_VALUE(Additive),
+		REF_VALUE(Multiplicative),
+		REF_VALUE(CutOut)
+	>
+REF_END;
+
+REFLECT(Jwl::DepthFunc)<>,
+	VALUES <
+		REF_VALUE(None),
+		REF_VALUE(WriteOnly),
+		REF_VALUE(TestOnly),
+		REF_VALUE(Normal)
+	>
+REF_END;

Engine/Jewel3D/Rendering/Text.h
@@ -9,6 +9,7 @@ namespace Jwl
 {
 	class Text : public Component<Text>
 	{
+		REFLECT_PRIVATE;
 	public:
 		Text(Entity& owner);
 		Text(Entity& owner, Font::Ptr font);
@@ -29,3 +30,13 @@ namespace Jwl
 		Font::Ptr data;
 	};
 }
+
+REFLECT(Jwl::Text) < Component >,
+	MEMBERS <
+		REF_MEMBER(text)<>,
+		REF_MEMBER(centeredX)<>,
+		REF_MEMBER(centeredY)<>,
+		REF_MEMBER(kernel)<>,
+		REF_MEMBER(data)<>
+	>
+REF_END;

EmilianC/Gemcutter

Engine/Jewel3D/Rendering/Rendering.h
@@ -1,5 +1,7 @@
 // Copyright (c) 2017 Emilian Cioca
 #pragma once
+#include "Jewel3D/Reflection/Reflection.h"
+
 #include <string_view>
 
 namespace Jwl
@@ -193,3 +195,65 @@ namespace Jwl
 		unsigned maxDrawBuffers = 0;
 	};
 }
+
+REFLECT(Jwl::TextureFormat)<>,
+	VALUES <
+		REF_VALUE(RGB_8),
+		REF_VALUE(RGB_16),
+		REF_VALUE(RGB_16F),
+		REF_VALUE(RGB_32),
+		REF_VALUE(RGB_32F),
+		REF_VALUE(RGBA_8),
+		REF_VALUE(RGBA_16),
+		REF_VALUE(RGBA_16F),
+		REF_VALUE(RGBA_32),
+		REF_VALUE(RGBA_32F),
+		REF_VALUE(DEPTH_24)
+	>
+REF_END;
+
+REFLECT(Jwl::TextureWrap)<>,
+	VALUES <
+		REF_VALUE(Clamp),
+		REF_VALUE(ClampWithBorder),
+		REF_VALUE(Repeat),
+		REF_VALUE(RepeatMirrored),
+		REF_VALUE(RepeatMirroredOnce)
+	>
+REF_END;
+
+REFLECT(Jwl::TextureFilter)<>,
+	VALUES <
+		REF_VALUE(Point),
+		REF_VALUE(Linear),
+		REF_VALUE(Bilinear),
+		REF_VALUE(Trilinear)
+	>
+REF_END;
+
+REFLECT(Jwl::CullFunc)<>,
+	VALUES <
+		REF_VALUE(None),
+		REF_VALUE(Clockwise),
+		REF_VALUE(CounterClockwise)
+	>
+REF_END;
+
+REFLECT(Jwl::BlendFunc)<>,
+	VALUES <
+		REF_VALUE(None),
+		REF_VALUE(Linear),
+		REF_VALUE(Additive),
+		REF_VALUE(Multiplicative),
+		REF_VALUE(CutOut)
+	>
+REF_END;
+
+REFLECT(Jwl::DepthFunc)<>,
+	VALUES <
+		REF_VALUE(None),
+		REF_VALUE(WriteOnly),
+		REF_VALUE(TestOnly),
+		REF_VALUE(Normal)
+	>
+REF_END;

Engine/Jewel3D/Rendering/Text.h
@@ -10,6 +10,7 @@ namespace Jwl
 	// Causes text to render at the entity's position.
 	class Text : public Component<Text>
 	{
+		REFLECT_PRIVATE;
 	public:
 		Text(Entity& owner);
 		Text(Entity& owner, Font::Ptr font);
@@ -27,3 +28,13 @@ namespace Jwl
 		float kernel = 1.0f;
 	};
 }
+
+REFLECT(Jwl::Text) < Component >,
+	MEMBERS <
+		REF_MEMBER(font)<>,
+		REF_MEMBER(text)<>,
+		REF_MEMBER(centeredX)<>,
+		REF_MEMBER(centeredY)<>,
+		REF_MEMBER(kernel)<>
+	>
+REF_END;